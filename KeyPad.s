9b55125a9e2ace912b4ab86c3a2a03a7b222ab32
rs1122 <rs1122@skbl-418-003.ph.ic.ac.uk>
2025-02-21 11:55:30 +0000

    Keypad Code position finder finished (pt 3)

--- KeyPad.s
+++ KeyPad.s
@@ -3,9 +3,6 @@
 global  keyPad_Setup, keyPad_Read, delay, delay_count
 
 psect	udata_acs   ; reserve data space in access ram
-column_pos: ds 1
-row_pos:    ds 1
-full_pos:   ds 1
 pos:	    ds 1
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
@@ -16,34 +13,20 @@ keyPad_Setup:
     movlb   0x00
     clrf    LATE, A	; 
     clrf    TRISD, A
-    ;clrf    TRISF, A
-    ;clrf    TRISH, A
     return
 
 keyPad_Read:
     movlw   0x0F
     movwf   TRISE, A
     call    delay
-    movff   PORTE, column_pos
-    call    delay
+    movff   PORTE, pos
     
     movlw   0xF0
     movwf   TRISE, A
     call    delay
-    movff   PORTE, row_pos
-    call    delay
-
-    ;movff   column_pos, PORTD
-    ;call    delay
-    ;movff   row_pos, PORTF
-    ;call    delay
-
-    movf    column_pos,	w, A
-    addwf   pos, f, A
-    movf    row_pos, w, A
-    addwf   pos, f, A
-
-    call    delay
+    movf    PORTE, w, A
+    
+    iorwf   pos
     movff   pos, PORTD
     clrf    pos, A
     
