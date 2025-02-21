40be726be950ea55547e44c00a8d877ae70d20c4
rs1122 <rs1122@skbl-418-003.ph.ic.ac.uk>
2025-02-21 11:29:30 +0000

    Keypad Code position finder finished

--- KeyPad.s
+++ KeyPad.s
@@ -1,23 +1,56 @@
 #include <xc.inc>
     
-global  keyPad_Setup, keyPad_Read
+global  keyPad_Setup, keyPad_Read, delay, delay_count
 
+psect	udata_acs   ; reserve data space in access ram
+column_pos: ds 1
+row_pos:    ds 1
+full_pos:   ds 1
+pos:	    ds 1
+delay_count:ds 1    ; reserve one byte for counter in the delay routine
+    
 psect	keyPad_code,class=CODE
 keyPad_Setup:
     movlb   0x0F	; select bank 15 (contains PADCFG1)
     bsf	    REPU	; turn on pull-ups
-    clrf    LATE	; 
-    clrf    TRISD
-    clrf    TRISF
-
+    movlb   0x00
+    clrf    LATE, A	; 
+    clrf    TRISD, A
+    ;clrf    TRISF, A
+    ;clrf    TRISH, A
     return
 
-keyPad_Read:	; Add Delay
+keyPad_Read:
     movlw   0x0F
-    movwf   TRISE
-    movff   PORTE,  PORTD
+    movwf   TRISE, A
+    call    delay
+    movff   PORTE, column_pos
+    call    delay
+    
     movlw   0xF0
-    movwf   TRISE
-    movff   PORTE,  PORTF
+    movwf   TRISE, A
+    call    delay
+    movff   PORTE, row_pos
+    call    delay
 
+    ;movff   column_pos, PORTD
+    ;call    delay
+    ;movff   row_pos, PORTF
+    ;call    delay
+
+    clrf    pos, A
+    movf    column_pos,	w, A
+    addwf   pos, f, A
+    movf    row_pos, w, A
+    addwf   pos, f, A
+    call    delay
+    movff   pos, PORTD
+    
     return
+
+delay:
+    decfsz  delay_count, A	; decrement until zero
+    bra	    delay
+    return
+
+    end
--- main.s
+++ main.s
@@ -2,11 +2,10 @@
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
 extrn	LCD_Setup, LCD_Write_Message
-extrn	keyPad_Setup, keyPad_Read
+extrn	keyPad_Setup, keyPad_Read, delay, delay_count
 	
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
@@ -24,13 +23,15 @@ rst: 	org 0x0
  	goto	setup
 
 	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup UART
+setup:	;bcf	CFGS	; point to Flash program memory  
+	;bsf	EEPGD 	; access Flash program memory
+	;call	UART_Setup	; setup UART
+	;call	LCD_Setup	; setup UART
 	call	keyPad_Setup
+read:
 	call	keyPad_Read
-	goto	setup
+	call	delay
+	goto	read
 	
 	; ******* Main programme ****************************************
 start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
@@ -58,9 +59,3 @@ loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 
 	goto	$		; goto current line in code
 
-	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
-	return
-
-	end	rst
