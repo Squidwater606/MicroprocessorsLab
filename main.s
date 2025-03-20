d9072064ae3367b426d34aca5d1cb6eed246f2a0
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 15:04:07 +0000

    Update main.s

--- main.s
+++ main.s
@@ -1,16 +1,17 @@
 #include <xc.inc>
 
-extrn	Phase_Setup_1, IO_Setup_1, Lookup_Setup_1, NCO_Int_Hi_1
-extrn	Phase_Setup_2, IO_Setup_2, Lookup_Setup_2, NCO_Int_Hi_2
-extrn	Phase_Setup_3, IO_Setup_3, Lookup_Setup_3, NCO_Int_Hi_3
-extrn	Phase_Setup_4, IO_Setup_4, Lookup_Setup_4, NCO_Int_Hi_4
+extrn	Phase_Setup_1, IO_Setup_1, Lookup_Setup_1
+extrn	Phase_Setup_2, IO_Setup_2, Lookup_Setup_2
+extrn	Phase_Setup_3, IO_Setup_3, Lookup_Setup_3
+extrn	Phase_Setup_4, IO_Setup_4, Lookup_Setup_4
+extrn	NCO_Int_Hi
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
 	goto	Start
 
 Int_Hi:	org	0x0008	; high vector, no low vector
-	goto	DDS_Int_Hi_1
+	goto	NCO_Int_Hi
 	
 Start:	call	Timer_Setup
 	call	Phase_Setup_1
