9218d9e54bcf8409c94c6455de28044997451c67
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 14:27:25 +0000

    Update main.s

--- main.s
+++ main.s
@@ -1,17 +1,25 @@
 #include <xc.inc>
 
-extrn	Phase_Setup, Timer_Setup, Lookup_Setup, DDS_Int_Hi
+extrn	Phase_Setup_1, IO_Setup_1, Lookup_Setup_1, DDS_Int_Hi_1
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
 	goto	Start
 
 Int_Hi:	org	0x0008	; high vector, no low vector
-	goto	DDS_Int_Hi
+	goto	DDS_Int_Hi_1
 	
-Start:	call	Phase_Setup
-	call	Timer_Setup
-	call	Lookup_Setup
+Start:	call	Timer_Setup
+	call	Phase_Setup_1
+	call	IO_Setup_1
+	call	Lookup_Setup_1
 	goto	$	; Sit in infinite loop
+
+Timer_Setup:
+	movlw	11001000B	; Set timer0 to 8-bit, Fosc/4
+	movwf	T0CON,	A	; = 16MHz clock rate, approx 4ms rollover
+	bsf	TMR0IE		; Enable timer0 interrupt
+	bsf	GIE		; Enable all interrupts
+	return
 
     end	    rst
