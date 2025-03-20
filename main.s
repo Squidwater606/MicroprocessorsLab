e896da3ce410de469eb2b1f3d676011a01321a7c
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 14:42:29 +0000

    Update main.s

--- main.s
+++ main.s
@@ -1,9 +1,9 @@
 #include <xc.inc>
 
-extrn	Phase_Setup_1, IO_Setup_1, Lookup_Setup_1, DDS_Int_Hi_1
-extrn	Phase_Setup_2, IO_Setup_2, Lookup_Setup_2, DDS_Int_Hi_2
-extrn	Phase_Setup_3, IO_Setup_3, Lookup_Setup_3, DDS_Int_Hi_3
-extrn	Phase_Setup_4, IO_Setup_4, Lookup_Setup_4, DDS_Int_Hi_4
+extrn	Phase_Setup_1, IO_Setup_1, Lookup_Setup_1, NCO_Int_Hi_1
+extrn	Phase_Setup_2, IO_Setup_2, Lookup_Setup_2, NCO_Int_Hi_2
+extrn	Phase_Setup_3, IO_Setup_3, Lookup_Setup_3, NCO_Int_Hi_3
+extrn	Phase_Setup_4, IO_Setup_4, Lookup_Setup_4, NCO_Int_Hi_4
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
