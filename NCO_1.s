b0a8e1f361b66bfdba2afa6c0de56f8c29686cf3
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 15:14:31 +0000

    Update NCO_1.s

--- NCO_1.s
+++ NCO_1.s
@@ -39,39 +39,4 @@ Lookup_Init_1:
 	movwf	Lookup_Ptr_1,	   A		; load low byte to TBLPTRL
 	return
 
-NCO_Int_Hi_1:	
-	btfss	TMR0IF		; check that this is timer0 interrupt
-	retfie	f		; if not then return
-Pointer_Ld_1:
-	movf	Lookup_Ptr_1 + 2, W, A
-	movwf	TBLPTRU,	   A
-	movf	Lookup_Ptr_1 + 1, W, A
-	movwf	TBLPTRH,	   A
-	movf	Lookup_Ptr_1,	W, A
-	movwf	TBLPTRL,	   A
-Phase_Amp_1:
-	tblrd*
-	movff	TABLAT, LATJ
-Re_Init_1:
-	movlw	low highword(Lookup_Table)	; address of data in PM
-	movwf	Lookup_Ptr_1 + 2,    A		; load upper bits to TBLPTRU
-	movlw	high(Lookup_Table)		; address of data in PM
-	movwf	Lookup_Ptr_1 + 1,    A		; load high byte to TBLPTRH
-	movlw	low(Lookup_Table)		; address of data in PM
-	movwf	Lookup_Ptr_1,	   A		; load low byte to TBLPTRL
-Phase_Inc_1:
-	movf	Phase_Jump_1, W,	A
-	addwf	Phase_Accum_1,	A
-	movf	Phase_Jump_1 + 1,	W,  A
-	addwfc	Phase_Accum_1 + 1,    A
-Pointer_Inc_1:
-	movf	Phase_Accum_1,	W,  A
-	addwf	Lookup_Ptr_1,	A
-	movf	Phase_Accum_1 + 1,	W,  A
-	addwfc	Lookup_Ptr_1 + 1,	A
-	movlw	0x00
-	addwfc	Lookup_Ptr_1 + 2,	A
-	bcf	TMR0IF		; clear interrupt flag
-	retfie	f		; fast return from interrupt
-
 	end
