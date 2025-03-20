0c84ee859cd06018c55325805260c91b7357f8d6
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 11:28:58 +0000

    Finished NCO.s

--- NCO.s
+++ NCO.s
@@ -12,12 +12,11 @@ Lookup_Ptr:	ds  3
 psect	dac_code, class=CODE
 
 Phase_Setup:
-	clrf	Phase_Accum + 2,    A
 	clrf	Phase_Accum + 1,    A
 	clrf	Phase_Accum,	    A
 	clrf	Phase_Jump + 1,	    A
 	clrf	Phase_Jump,	    A
-	movlw	0x01
+	movlw	0xFF
 	movwf	Phase_Jump,	    A
 	movlw	0x00
 	movwf	Phase_Jump + 1,	    A
@@ -26,7 +25,7 @@ Phase_Setup:
 Timer_Setup:
 	clrf	TRISJ,	A	; Set PORTD as all outputs
 	clrf	LATJ,	A	; Clear PORTD outputs
-	movlw	10001000B	; Set timer0 to 16-bit, Fosc/4
+	movlw	11001000B	; Set timer0 to 8-bit, Fosc/4
 	movwf	T0CON,	A	; = 16MHz clock rate, approx 4ms rollover
 	bsf	TMR0IE		; Enable timer0 interrupt
 	bsf	GIE		; Enable all interrupts
@@ -48,23 +47,28 @@ DDS_Int_Hi:
 	btfss	TMR0IF		; check that this is timer0 interrupt
 	retfie	f		; if not then return
 Pointer_Ld:
-	;movf	Lookup_Ptr + 2, W, A
-	;movwf	TBLPTRU,	   A
-	;movf	Lookup_Ptr + 1, W, A
-	;movwf	TBLPTRH,	   A
-	;movf	Lookup_Ptr,	W, A
-	;movwf	TBLPTRL,	   A
+	movf	Lookup_Ptr + 2, W, A
+	movwf	TBLPTRU,	   A
+	movf	Lookup_Ptr + 1, W, A
+	movwf	TBLPTRH,	   A
+	movf	Lookup_Ptr,	W, A
+	movwf	TBLPTRL,	   A
 Phase_Amp:
-	;tblrd*
-	;movff	TABLAT, LATJ
+	tblrd*
+	movff	TABLAT, LATJ
+Re_Init:
+	movlw	low highword(Lookup_Table)	; address of data in PM
+	movwf	Lookup_Ptr + 2,    A		; load upper bits to TBLPTRU
+	movlw	high(Lookup_Table)		; address of data in PM
+	movwf	Lookup_Ptr + 1,    A		; load high byte to TBLPTRH
+	movlw	low(Lookup_Table)		; address of data in PM
+	movwf	Lookup_Ptr,	   A		; load low byte to TBLPTRL
 Phase_Inc:
-	;incf	Lookup_Ptr, A
 	movf	Phase_Jump, W,	A
 	addwf	Phase_Accum,	A
 	movf	Phase_Jump + 1,	W,  A
 	addwfc	Phase_Accum + 1,    A
 Pointer_Inc:
-	bc	Lookup_Re_Init
 	movf	Phase_Accum,	W,  A
 	addwf	Lookup_Ptr,	A
 	movf	Phase_Accum + 1,W,  A
@@ -73,15 +77,5 @@ Pointer_Inc:
 	addwfc	Lookup_Ptr + 2,	A
 	bcf	TMR0IF		; clear interrupt flag
 	retfie	f		; fast return from interrupt
-
-Lookup_Re_Init:
-	movlw	low highword(Lookup_Table)	; address of data in PM
-	movwf	Lookup_Ptr + 2,    A		; load upper bits to TBLPTRU
-	movlw	high(Lookup_Table)		; address of data in PM
-	movwf	Lookup_Ptr + 1,    A		; load high byte to TBLPTRH
-	movlw	low(Lookup_Table)		; address of data in PM
-	movwf	Lookup_Ptr,	   A		; load low byte to TBLPTRL
-	bcf	STATUS, 0
-	goto	Pointer_Inc
 
 	end
