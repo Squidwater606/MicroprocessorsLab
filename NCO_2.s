b4b6f26a9938c4815012e7d62aa5a80a3d5aaa69
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 15:14:11 +0000

    Update NCO_2.s

--- NCO_2.s
+++ NCO_2.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 	
-global	Phase_Setup_2, IO_Setup_2, Lookup_Setup_2, NCO_Int_Hi_2  ; global routines
+global	Phase_Setup_2, IO_Setup_2, Lookup_Setup_2  ; global routines
 global	Phase_Jump_2, Phase_Accum_2, Lookup_Ptr_2 ; global variables
 extrn	Lookup_Table	; global data
 
@@ -38,40 +38,5 @@ Lookup_Init_2:
 	movlw	low(Lookup_Table)		; address of data in PM
 	movwf	Lookup_Ptr_2,	   A		; load low byte to TBLPTRL
 	return
-
-NCO_Int_Hi_2:	
-	btfss	TMR0IF		; check that this is timer0 interrupt
-	retfie	f		; if not then return
-Pointer_Ld_2:
-	movf	Lookup_Ptr_2 + 2, W, A
-	movwf	TBLPTRU,	   A
-	movf	Lookup_Ptr_2 + 1, W, A
-	movwf	TBLPTRH,	   A
-	movf	Lookup_Ptr_2,	W, A
-	movwf	TBLPTRL,	   A
-Phase_Amp_2:
-	tblrd*
-	movff	TABLAT, LATH
-Re_Init_2:
-	movlw	low highword(Lookup_Table)	; address of data in PM
-	movwf	Lookup_Ptr_2 + 2,    A		; load upper bits to TBLPTRU
-	movlw	high(Lookup_Table)		; address of data in PM
-	movwf	Lookup_Ptr_2 + 1,    A		; load high byte to TBLPTRH
-	movlw	low(Lookup_Table)		; address of data in PM
-	movwf	Lookup_Ptr_2,	   A		; load low byte to TBLPTRL
-Phase_Inc_2:
-	movf	Phase_Jump_2, W,	A
-	addwf	Phase_Accum_2,	A
-	movf	Phase_Jump_2 + 1,	W,  A
-	addwfc	Phase_Accum_2 + 1,    A
-Pointer_Inc_2:
-	movf	Phase_Accum_2,	W,  A
-	addwf	Lookup_Ptr_2,	A
-	movf	Phase_Accum_2 + 1,	W,  A
-	addwfc	Lookup_Ptr_2 + 1,	A
-	movlw	0x00
-	addwfc	Lookup_Ptr_2 + 2,	A
-	bcf	TMR0IF		; clear interrupt flag
-	retfie	f		; fast return from interrupt
 
 	end
