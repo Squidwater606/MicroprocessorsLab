9d8a586c725bc762381262a8d8c1e5d0a60385bb
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 14:49:51 +0000

    Update and rename DDS_Int_Hi.s to NCO_Int_Hi.s

--- NCO_Int_Hi.s
+++ NCO_Int_Hi.s
@@ -80,5 +80,40 @@ Pointer_Inc_2:
 	bcf	TMR0IF		; clear interrupt flag
 	retfie	f		; fast return from interrupt
 
+ NCO_Int_Hi_3:	
+	btfss	TMR0IF		; check that this is timer0 interrupt
+	retfie	f		; if not then return
+Pointer_Ld_3:
+	movf	Lookup_Ptr_3 + 2, W, A
+	movwf	TBLPTRU,	   A
+	movf	Lookup_Ptr_3 + 1, W, A
+	movwf	TBLPTRH,	   A
+	movf	Lookup_Ptr_3,	W, A
+	movwf	TBLPTRL,	   A
+Phase_Amp_3:
+	tblrd*
+	movff	TABLAT, LATG
+Re_Init_3:
+	movlw	low highword(Lookup_Table)	; address of data in PM
+	movwf	Lookup_Ptr_3 + 2,    A		; load upper bits to TBLPTRU
+	movlw	high(Lookup_Table)		; address of data in PM
+	movwf	Lookup_Ptr_3 + 1,    A		; load high byte to TBLPTRH
+	movlw	low(Lookup_Table)		; address of data in PM
+	movwf	Lookup_Ptr_3,	   A		; load low byte to TBLPTRL
+Phase_Inc_3:
+	movf	Phase_Jump_3, W,	A
+	addwf	Phase_Accum_3,	A
+	movf	Phase_Jump_3 + 1,	W,  A
+	addwfc	Phase_Accum_3 + 1,    A
+Pointer_Inc_3:
+	movf	Phase_Accum_3,	W,  A
+	addwf	Lookup_Ptr_3,	A
+	movf	Phase_Accum_3 + 1,	W,  A
+	addwfc	Lookup_Ptr_3 + 1,	A
+	movlw	0x00
+	addwfc	Lookup_Ptr_3 + 2,	A
+	bcf	TMR0IF		; clear interrupt flag
+	retfie	f		; fast return from interrupt
+
 	end
 
