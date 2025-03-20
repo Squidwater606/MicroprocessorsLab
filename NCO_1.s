26a817a4bd0a76836fdad5d6f7b6b9cb7890b41c
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-20 15:08:39 +0000

    Update NCO_1.s

--- NCO_1.s
+++ NCO_1.s
@@ -1,6 +1,7 @@
 #include <xc.inc>
 	
 global	Phase_Setup_1, IO_Setup_1, Lookup_Setup_1, NCO_Int_Hi_1  ; global routines
+global	Phase_Jump_1, Phase_Accum_1, Lookup_Ptr_1 ; global variables
 extrn	Lookup_Table	; global data
 
 psect	udata_acs   ; reserve data space in access ram
