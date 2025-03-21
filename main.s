df1e3f665f5b37978790e23ecae6dc618f1be2e7
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-21 09:58:35 +0000

    Update main.s

--- main.s
+++ main.s
@@ -1,17 +1,42 @@
 #include <xc.inc>
 
-extrn	Phase_Setup, Timer_Setup, Lookup_Setup, DDS_Int_Hi
+extrn	Phase_Setup_1, IO_Setup_1, Lookup_Setup_1
+extrn	Phase_Setup_2, IO_Setup_2, Lookup_Setup_2
+extrn	Phase_Setup_3, IO_Setup_3, Lookup_Setup_3
+extrn	Phase_Setup_4, IO_Setup_4, Lookup_Setup_4
+extrn	keyPad_Setup, keyPad_Read
+extrn	NCO_Int_Hi
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
 	goto	Start
 
 Int_Hi:	org	0x0008	; high vector, no low vector
-	goto	DDS_Int_Hi
+	goto	NCO_Int_Hi
 	
-Start:	call	Phase_Setup
-	call	Timer_Setup
-	call	Lookup_Setup
-	goto	$	; Sit in infinite loop
+Start:	call	Timer_Setup
+	call	Phase_Setup_1
+ 	call	Phase_Setup_2
+  	call	Phase_Setup_3
+   	call	Phase_Setup_4
+	call	IO_Setup_1
+ 	call	IO_Setup_2
+  	call	IO_Setup_3
+   	call	IO_Setup_4
+	call	Lookup_Setup_1
+ 	call	Lookup_Setup_2
+	call	Lookup_Setup_3
+ 	call	Lookup_Setup_4
+	call	keyPad_Setup
+loop:
+	call	keyPad_Read
+	goto	loop
+
+Timer_Setup:
+	movlw	11001000B	; Set timer0 to 8-bit, Fosc/4
+	movwf	T0CON,	A	; = 16MHz clock rate, approx 4ms rollover
+	bsf	TMR0IE		; Enable timer0 interrupt
+	bsf	GIE		; Enable all interrupts
+	return
 
     end	    rst
