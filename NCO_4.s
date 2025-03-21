68d1be9a87a769acb6237c29c7a6a4a4f9dd3efb
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-21 09:59:42 +0000

    Create NCO_4.s

---
+++ NCO_4.s
@@ -0,0 +1,42 @@
+#include <xc.inc>
+	
+global	Phase_Setup_4, IO_Setup_4, Lookup_Setup_4  ; global routines
+global	Phase_Jump_4, Phase_Accum_4, Lookup_Ptr_4 ; global variables
+extrn	Lookup_Table	; global data
+extrn	Phase_Jump_4
+
+psect	udata_acs   ; reserve data space in access ram
+Phase_Accum_4:	ds  2
+Lookup_Ptr_4:	ds  3
+
+psect	nco_code_4, class=CODE
+
+Phase_Setup_4:
+	clrf	Phase_Accum_4 + 1,    A
+	clrf	Phase_Accum_4,	    A
+	clrf	Phase_Jump_4 + 1,	    A
+	clrf	Phase_Jump_4,	    A
+	movlw	0x3F
+	movwf	Phase_Jump_4,	    A
+	movlw	0x03
+	movwf	Phase_Jump_4 + 1,	    A
+	return
+
+IO_Setup_4:
+	clrf	TRISD,	A	; Set PORTD as all outputs
+	clrf	LATD,	A	; Clear PORTD outputs
+ 	return
+
+Lookup_Setup_4:
+	bcf	CFGS			; point to Flash program memory  
+	bsf	EEPGD			; access Flash program memory
+Lookup_Init_4:
+	movlw	low highword(Lookup_Table)	; address of data in PM
+	movwf	Lookup_Ptr_4 + 2,    A		; load upper bits to TBLPTRU
+	movlw	high(Lookup_Table)		; address of data in PM
+	movwf	Lookup_Ptr_4 + 1,    A		; load high byte to TBLPTRH
+	movlw	low(Lookup_Table)		; address of data in PM
+	movwf	Lookup_Ptr_4,	   A		; load low byte to TBLPTRL
+	return
+
+	end
